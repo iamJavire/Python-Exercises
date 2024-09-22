087ea865a8404ced56c057a98869f8c7e3f41133
iamJavire <javicrurod@gmail.com>
2024-09-22 11:30:47 +0200

    Create suma_dos_numeros.py

---
+++ Nivel 1/suma_dos_numeros.py
@@ -0,0 +1,4 @@
+num1 = int(input("Introduce el primer número a sumar: "))
+num2 = int(input("Introduce el segundo número a sumar: "))
+suma = num1 + num2
+print("La suma de "+str(num1)+" más "+str(num2)+" es un total de "+str(suma))
