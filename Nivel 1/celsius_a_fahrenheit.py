9e19be7dde707008a27af415ec4c93ef53585a0b
iamJavire <javicrurod@gmail.com>
2024-09-22 11:36:20 +0200

    Create celsius_a_fahrenheit.py

---
+++ Nivel 1/celsius_a_fahrenheit.py
@@ -0,0 +1,4 @@
+numCelscius = float(input("Introduce los grados en Celcios: "))
+numFahrenheit = (numCelscius*9/5)+32
+### print("Los "+str(numCelscius)+" celcios son "+str(numFahrenheit)+" faradais")
+print(f"Los {numCelscius} Celcios son {numFahrenheit} Faradais")
