464674c55228bfdf0fcddda9128d2d1232574b32
iamJavire <javicrurod@gmail.com>
2024-09-22 11:51:57 +0200

    Create calcular_edad.py

---
+++ Nivel 1/calcular_edad.py
@@ -0,0 +1,5 @@
+nombre = input("Introduce el nombre: ")
+anoNacimiento = int(input("Introduce el año de nacimiento: "))
+anoActual = 2024
+edadActual = anoActual-anoNacimiento
+print(f"{nombre} tiene actualmente {edadActual} años.")
