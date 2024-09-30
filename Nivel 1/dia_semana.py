12a70f753d44929b3d47bcd04eb3c8015ae1bde1
iamJavire <javicrurod@gmail.com>
2024-09-30 07:24:36 +0200

    Create dia_semana.py

---
+++ Nivel 1/dia_semana.py
@@ -0,0 +1,15 @@
+numSemana = int(input("Introduce un número de 1 al 7: "))
+if numSemana == 1:
+    print("Es lunes")
+elif numSemana == 2:
+    print("Es martes")
+elif numSemana == 3:
+    print("Es miércoles")
+elif numSemana == 4:
+    print("Es jueves")
+elif numSemana == 5:
+    print("Es viernes")
+elif numSemana == 6:
+    print("Es sábado")
+else:
+    print("Es domingo")
