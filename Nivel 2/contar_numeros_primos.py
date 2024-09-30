9226bea50b035d8cede518388c94aa0eeec2ee5f
iamJavire <javicrurod@gmail.com>
2024-09-30 07:38:01 +0200

    Create contar_numeros_primos.py

---
+++ Nivel 2/contar_numeros_primos.py
@@ -0,0 +1,28 @@
+def es_primo(num):
+    if num <= 1:
+        return False
+    if num == 1:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range (3, int(num**0.5)+1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
+numEntero = input("Introduce un número o 'fin': ")
+listaEnteros = []
+qPrimos = 0
+
+while numEntero != "fin":
+    listaEnteros.append(int(numEntero))
+    numEntero = input("Introduce un número o 'fin': ")
+
+for num in listaEnteros:
+    if es_primo(num):
+        qPrimos += 1
+    else:
+        qPrimos += 0
+
+print(f"Hay {qPrimos} números primos")
